ab4deb21b4a147a28f65e8f67690866b791913d0
Jithendra V Anand <125388931+JithuMon10@users.noreply.github.com>
2025-10-07 12:10:00 +0530

    feat: add GHMatrixSyncTest.java

---
+++ graphhopper/client-hc/src/test/java/com/graphhopper/api/GHMatrixSyncTest.java
@@ -0,0 +1,36 @@
+package com.graphhopper.api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author Peter Karich
+ */
+public class GHMatrixSyncTest extends [REDACTED] {
+
+    @Override
+    [REDACTED] createMatrixClient(String jsonStr, int errorCode) throws IOException {
+        JsonNode json = objectMapper.readTree(jsonStr);
+
+        // for test we grab the solution from the "batch json"
+        if (json.has("solution")) {
+            json = json.get("solution");
+        }
+
+        final String finalJsonStr = json.toString();
+        return new [REDACTED](new [REDACTED]("") {
+
+            @Override
+            protected JsonResult postJson(String url, JsonNode data) {
+                return new JsonResult(finalJsonStr, errorCode, new HashMap<>());
+            }
+        });
+    }
+
+    @Override
+    [REDACTED] createRequester(String url) {
+        return new [REDACTED](url);
+    }
+}
